c932884a86ed3c82541006e535d2c8eeb8a813fb
PunitDh <cas012115@coderacademy.edu.au>
2021-05-11 16:18:32 +1000

    add devise

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 # before_action @post = Post.find(params[:id])
-
+    before_action :authenticate_user!
+    
     def index
         @posts = Post.all
     end
